850e85fafd3edb8e33e6147afada01d631fcb16e
Jzridar <jzridar@gmail.com>
2023-03-29 15:36:31 -0400

    Readme file updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const container = document.querySelector('.container')
+let contDimension = container.getBoundingClientRect()
 const gameover = document.querySelector('.gameover')
 const ball = document.querySelector('.ball')
 const paddle = document.querySelector('.paddle')
@@ -15,4 +16,14 @@ container.appendChild(paddle)
 //creating a funtion to atart game when the gameover el is clicked
 function startgame() {
     console.log('start')
+    setupBricks(30)
+}
+
+function setupBricks(num){
+    let row = {
+        x:((contDimension.width % 200)/2),
+      
+        y:50
+    }
+    console.log(row)
 }
